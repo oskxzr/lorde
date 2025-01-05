fd53908d5ddbc9d7bf448365ecf3cf70662b1dbe
Amy Oliver Official <oskargreig@gmail.com>
2025-01-05 16:01:22 +1300

    Update pages.py

--- modules/pages/pages.py
+++ modules/pages/pages.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, jsonify, session
 import os
 import requests
 from modules.pages.media import Media
+import time
 
 pages = Blueprint("pages", __name__)
 
@@ -10,6 +11,63 @@ titles = dbutils.titles
 watch_history = dbutils.watch_history
 
 title_data = {}
+
+def get_next_episode(show_id, season, episode):
+    season_num = int(season[1:])
+    episode_num = int(episode[1:])
+    
+    current_season_episodes = len(title_data[show_id.lower()]["seasons"][str(season_num)])
+    
+    if episode_num < current_season_episodes:
+        # Next episode in the same season
+        return {
+            "season": season,
+            "episode": f"E{(episode_num + 1):02}",
+            "timestamp": 0
+        }
+    elif str(season_num + 1) in title_data[show_id]["seasons"]:
+        # First episode of the next season
+        return {
+            "season": f"s{(season_num + 1):02}",
+            "episode": "E1",
+            "timestamp": 0
+        }
+    # No next episode available
+    return None
+
+def create_continue_watching(user_watch_history):
+    continue_watching = {}
+    for key in user_watch_history:
+        data = user_watch_history[key].copy()
+        
+        split = key.split("_")
+        if len(split) > 1:
+            # For TV shows
+            show_id = split[0]
+            data["id"] = show_id
+            data["season"] = split[1]
+            data["episode"] = split[2]
+            
+            season_num = int(data["season"][1:])
+            episode_num = int(data["episode"][1:])
+            
+            episode_length = title_data[show_id]["seasons"][str(season_num)][episode_num]["time"]["m"]
+            minutes_remaining = ((episode_length) - (float(data["timestamp"]) / 60))
+            
+            # If episode is >95% complete, set up the next episode
+            if minutes_remaining <= 1.5:
+                next_episode_data = get_next_episode(show_id, data["season"], data["episode"])
+                if next_episode_data:
+                    data.update(next_episode_data)
+            
+            # Only update if this episode was watched later than the existing one
+            if show_id not in continue_watching or data["time"] > continue_watching[show_id]["time"]:
+                continue_watching[show_id] = data
+        else:
+            # For movies
+            data["id"] = key
+            continue_watching[key] = data
+    return {k: v for k, v in sorted(continue_watching.items(), key=lambda item: item[1]["time"], reverse=True)}
 
 def update_title_data():
     print("getting title data...")
@@ -25,7 +83,7 @@ def update_title_data():
             for season in entry["seasons"]:
                 media_data["seasons"][season] = [episode.get_metadata() for episode in media.get_episodes(int(season))]
 
-        local_title_data[entry["name"]] = media_data
+        local_title_data[entry["_id"]] = media_data
     global title_data
     title_data = local_title_data
     print("finished")
@@ -44,7 +102,13 @@ def test_backend():
 def pages_index():
     # if not test_backend():
     #     return render_template("offline.html")
-    return render_template("home.html", title_data = title_data)
+    user_watch_history = {}
+    if session.get("user_data"):
+        user_watch_history = watch_history.find(session.get("user_data")["_id"])
+    if user_watch_history != {}:
+        del user_watch_history["_id"]
+    return render_template("home.html", title_data = title_data, watch_history = user_watch_history, continue_watching = create_continue_watching(user_watch_history))
+    # return render_template("home.html", title_data = title_data)
 
 def get_watch_history(user_id, id, season = None, episode = None):
     watch_history_data = watch_history.find(user_id)
@@ -63,17 +127,32 @@ def pages_watch(path):
     path = title.upper()
     season = None
     episode = None
+    subtitles = None
+    next_episode = None
+    if data["type"] == "MOVIE":
+        subtitles = f"/static/subtitles/{path}.vtt"
+    else:
+        season = split[1].upper()
+        episode = split[2].upper()
+        next_episode_data = get_next_episode(path, season, episode)
+        if next_episode_data:
+            next_episode = next_episode_data
+            next_episode["data"] = title_data[path.lower()]["seasons"][str(int(season[1:]))][int(episode[1:])]
+            next_episode["title"] = path
+        subtitles = f"/static/subtitles/{path}/{season}/{episode}.vtt"
+
     for quality in data["qualities"]:
         if data["type"] == "MOVIE":
-            tracks.append(f"{os.environ["CDN_BASE"]}/video/{path}/{quality}/{path}.mp4")
+            tracks.append({"src": f"{os.environ["CDN_BASE"]}/video/{path}/{quality}/{path}.mp4", 'quality': int(quality[:-1])})
         else:
-            season = split[1].upper()
-            episode = split[2].upper()
-            tracks.append(f"{os.environ["CDN_BASE"]}/video/{path}/{quality}/{season}/{episode}.mp4")
+            tracks.append({"src": f"{os.environ["CDN_BASE"]}/video/{path}/{quality}/{season}/{episode}.mp4", 'quality': int(quality[:-1])})
 
     last_watched = get_watch_history(session["user_data"]["_id"], path.lower(), season, episode)
-    print(last_watched)
-    return render_template("player.html", tracks = tracks, title_data = title_data[data["name"]], timestamp = last_watched)
+    if last_watched:
+        return render_template("player.html", tracks = tracks, title_data = title_data[path.lower()], timestamp = last_watched["timestamp"], subtitles = subtitles, next_episode = next_episode)
+    else:
+        return render_template("player.html", tracks = tracks, title_data = title_data[path.lower()], subtitles = subtitles, next_episode = next_episode)
+
 
 @pages.route("/resettitledata")
 def resettitledata():
@@ -89,16 +168,16 @@ def watchhistory():
     # or  /watch/breakingbad/s01/e01
     split = video_url.split("/")
     # 0 = "" 1 = "watch" 2 = id
-    id = split[2]
+    id = split[2].lower()
 
     data = titles.find(id)
 
     if data:
         if data["type"] == "MOVIE":
-            watch_history.set({"_id": session["user_data"]["_id"], id.lower(): timestamp})
+            watch_history.set({"_id": session["user_data"]["_id"], id: {"timestamp": timestamp, "time": time.time()}})
         else:
             id = f"{id.lower()}_{split[3].upper()}_{split[4].upper()}"
-            watch_history.set({"_id": session["user_data"]["_id"], id: timestamp})
+            watch_history.set({"_id": session["user_data"]["_id"], id: {"timestamp": timestamp, "time": time.time()}})
 
     # Send a response back to the client
     return "YES MAMA"
