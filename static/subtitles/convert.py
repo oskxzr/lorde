7fe4dbecf9c6bd742df7c5db911ac6f369794638
Amy Oliver Official <oskargreig@gmail.com>
2025-01-07 23:57:44 +1300

    subtitles!

--- static/subtitles/convert.py
+++ static/subtitles/convert.py
@@ -16,7 +16,7 @@ def convert_srt_to_vtt(file_path):
 def process_current_directory():
     """Process all .srt files in the current directory and its descendants."""
     current_directory = os.getcwd()  # Get the current working directory
-    for dirpath, _, filenames in os.walk(current_directory):
+    for dirpath, _, filenames in os.walk("C:/Users/oskar/OneDrive/Desktop/CODE PROJ/lorde/static/subtitles"):
         for filename in filenames:
             if filename.endswith('.srt'):
                 file_path = os.path.join(dirpath, filename)
